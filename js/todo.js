bfdfb19dd1b2f6c155b67ba459d897c9c5b93241
hellosylvee <helloimsylvia@gmail.com>
2017-05-05 14:12:39 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-  // call functions here
+
+console.log("document loaded")
+
+  $('form').on('submit', function(e){
+    var item = $('#item').val();
+    $('#list').append(`<li>${item}</li>`);
+    e.preventDefault()
+    console.log("hello you?????")
+  });
 });
 
 // define functions here
